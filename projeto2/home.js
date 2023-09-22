8436e5af1894e6c8a3f4142fd8e197a8e26c9545
Bianca Florencio <128937708+BiancaF16@users.noreply.github.com>
2023-09-22 16:54:36 -0300

    cart

---
+++ projeto2/home.js
@@ -0,0 +1,6 @@
+produtosJson.map((item, index ) => {
+    // console.log(item)
+
+    let roupaItem = document.querySelector('.produtos .roupaItem').cloneNode(true)
+    console.log(roupaItem)
+})
---
+++ projeto2/produtos.js
@@ -0,0 +1,18 @@
+let produtosJson = [
+    {
+        id: 0,
+        nome: 'camisa',
+        img: 'image/camisa.jpg',
+        price: 59.99
+
+    },
+
+
+{
+    id: 1,
+    nome: 'short',
+    img: 'image/short.webp',
+    price: 59.99
+
+}
+]
